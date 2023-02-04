2ea5581f3e5fdd55307f9f55ee846f0eb1e9e4ef
jinxianwei <81373517+jinxianwei@users.noreply.github.com>
2023-02-04 16:15:59 +0800

    Create split_data.py
    split_data of classification to format of ImageNet
    original
      class1 
           ...png
      class2
          ...png
       ...
    to the formate of 
    train.txt
    val.txt
    class.txt
    train
       class1
            ...png
       class2
        ...
    val
        ....

---
+++ split_data.py
@@ -0,0 +1,91 @@
+import os
+from shutil import copy, rmtree
+import random
+
+
+def mk_file(file_path: str):
+    if os.path.exists(file_path):
+        # 如果文件夹存在，则先删除原文件夹在重新创建
+        rmtree(file_path)
+    os.makedirs(file_path)
+
+
+def main():
+    # 保证随机可复现
+    random.seed(0)
+
+    # 将数据集中20%的数据划分到验证集中
+    split_rate = 0.2
+
+    # 指向你解压后的flower_photos文件夹
+    data_root = "/root/autodl-tmp/ImageNet_format"
+    origin_flower_path = '/root/autodl-tmp/flower_dataset'
+    assert os.path.exists(origin_flower_path), "path '{}' does not exist.".format(origin_flower_path)
+
+    flower_class = [cla for cla in os.listdir(origin_flower_path)
+                    if os.path.isdir(os.path.join(origin_flower_path, cla))]
+    flower_dict = {}
+    for i in range(len(flower_class)):
+        flower_dict[flower_class[i]] = i
+
+    # 建立保存训练集的文件夹
+    train_root = os.path.join(data_root, "train")
+    mk_file(train_root)
+    for cla in flower_class:
+        # 建立每个类别对应的文件夹
+        mk_file(os.path.join(train_root, cla))
+
+    # 建立保存验证集的文件夹
+    val_root = os.path.join(data_root, "val")
+    mk_file(val_root)
+    for cla in flower_class:
+        # 建立每个类别对应的文件夹
+        mk_file(os.path.join(val_root, cla))
+    train_list = []
+    val_list = []
+
+    for cla in flower_class:
+        cla_path = os.path.join(origin_flower_path, cla)
+        images = os.listdir(cla_path)
+        num = len(images)
+        # 随机采样验证集的索引
+        eval_index = random.sample(images, k=int(num*split_rate))
+        for index, image in enumerate(images):
+            if image in eval_index:
+                # 将分配至验证集中的文件复制到相应目录
+                image_path = os.path.join(cla_path, image)
+                new_path = os.path.join(val_root, cla)
+                current_class_name = image_path.split('/')[-2]
+                current_file_name = image_path.split('/')[-1]
+                
+                val_list.append(current_class_name + '/' + current_file_name + ' ' + str(flower_dict[current_class_name]) + '\n')
+                copy(image_path, new_path)
+            else:
+                # 将分配至训练集中的文件复制到相应目录
+                image_path = os.path.join(cla_path, image)
+                new_path = os.path.join(train_root, cla)
+                
+                
+                current_class_name = image_path.split('/')[-2]
+                current_file_name = image_path.split('/')[-1]
+                train_list.append(current_class_name + '/' + current_file_name + ' ' + str(flower_dict[current_class_name]) + '\n')
+                copy(image_path, new_path)
+            print("\r[{}] processing [{}/{}]".format(cla, index+1, num), end="")  # processing bar
+        print()
+        
+    with open(os.path.join(data_root, 'train.txt'), 'w') as f:
+        f.writelines(train_list)
+    with open(os.path.join(data_root, 'val.txt'), 'w') as f:
+        f.writelines(val_list)
+        
+    flower_class = [name + '\n' for name in flower_class ]
+    with open(os.path.join(data_root, 'classes.txt'), 'w') as f:
+        f.writelines(flower_class)
+        
+
+    print("processing done!")
+    
+
+
+if __name__ == '__main__':
+    main()
